c55d315be24d83f17557103f9290638798062e61
Warren Sadler <42621284+warren-sadler@users.noreply.github.com>
2021-12-12 13:47:33 -0600

    fix: move tests to remove from bundling

--- __tests__/components/Example/Example.spec.tsx
+++ __tests__/components/Example/Example.spec.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import { Example } from "./index";
+import { Example } from "../../../src/components/Example/index";
 
 describe("Example Component", () => {
   it("should render correctly", () => {
